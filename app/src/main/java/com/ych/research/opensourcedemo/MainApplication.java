bc2901a2ceb8fbe29ee1455dfd2139991f93f2df
YangCH0915 <yangch0915@sina.com>
2017-03-08 17:29:39 +0800

    提交更新

--- app/src/main/java/com/ych/research/opensourcedemo/MainApplication.java
+++ app/src/main/java/com/ych/research/opensourcedemo/MainApplication.java
@@ -3,6 +3,8 @@ package com.ych.research.opensourcedemo;
 import android.app.Application;
 import android.content.Context;
 
+import okhttp3.Request;
+
 /**
  * Created by Administrator on 2017/3/8 0008.
  */
@@ -11,10 +13,20 @@ public class MainApplication extends Application {
 
     public static Context mContext;
 
+    private Request request;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         mContext = this;
+    }
+
+
+    public Request getRequestInstance() {
+        if (request == null) {
+            request = new Request.Builder().build();
+        }
+        return request;
     }
 }
